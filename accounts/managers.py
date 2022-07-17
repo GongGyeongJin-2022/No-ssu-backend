0793e3d8a7dc1a5808ffd8ce3ef7e8e481d737d8
euichan2 <euichan1108@gmail.com>
2022-07-17 22:53:38 +0900

    small fix

--- accounts/managers.py
+++ accounts/managers.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, email, password, first_name, extra_fields):
+    def create_superuser(self, email, password, first_name, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
