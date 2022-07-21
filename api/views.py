03f2258e1db95a6ee5e0f60e894e7f312f579a0a
euichan2 <euichan1108@gmail.com>
2022-07-21 21:18:33 +0900

    set marker permission IsAuthenticatedOrReadOnly

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from .models import Marker
 from rest_framework import viewsets
 from .serializers import MarkerSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class MarkerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Marker.objects.all()
     serializer_class = MarkerSerializer
