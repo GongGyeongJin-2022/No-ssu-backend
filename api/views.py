eed6b118c55f8b3c3a2b5082612217e9e2fde094
PRESSO <caff1nepill@gmail.com>
2022-08-18 21:12:06 +0900

    add response message

--- api/views.py
+++ api/views.py
@@ -26,7 +26,7 @@ class MarkerViewSet(viewsets.ModelViewSet):
         marker.save()
         user.save()
 
-        return Response({"data": "success"}, status=status.HTTP_200_OK)
+        return Response({"status": "success"}, status=status.HTTP_200_OK)
 
 
 
