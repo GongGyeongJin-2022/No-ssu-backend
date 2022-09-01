049b35f8926c3592740f61b55b229e11b0c1008c
jihyunna <skwlgus106@naver.com>
2022-09-01 20:49:56 +0900

    add Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import field
 from rest_framework import serializers
 from .models import Marker, Reward
 
@@ -10,8 +11,41 @@ class RewardSerializer(serializers.ModelSerializer):
 
 class MarkerSerializer(serializers.ModelSerializer):
 
-    reward = RewardSerializer()
+    posted_user = serializers.ReadOnlyField
+
+    reward = RewardSerializer(many=False)
 
     class Meta:
         model = Marker
         fields = '__all__'
+
+    def create(self, validated_data):
+        reward_data = validated_data.pop('reward')
+        reward = Reward.objects.create(**reward_data)
+        tags = validated_data.pop('tags')
+        marker = Marker.objects.create(reward=reward, **validated_data)
+
+        for tag in tags:
+            marker.tags.add(tag)
+        return marker
+  
+    
+
+
+class MarkerSimpleSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Marker
+        fields = ("longitude","latitude","reward")
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    reward = RewardSerializer(many=False)
+
+    class Meta:
+        model = Marker
+        fields = ("longitude","latitude","reward")
+
+
+    
